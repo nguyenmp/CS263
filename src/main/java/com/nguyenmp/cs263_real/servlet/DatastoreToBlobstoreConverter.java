34dc212aa47cb4c65acfc3915a2616b4bf3ce64b
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-11-28 23:25:51 -0800

    Initial task of pushing datastore into blobstore

---
+++ src/main/java/com/nguyenmp/cs263_real/servlet/DatastoreToBlobstoreConverter.java
@@ -0,0 +1,85 @@
+package com.nguyenmp.cs263_real.servlet;
+
+import com.google.appengine.tools.cloudstorage.*;
+import com.google.gson.Gson;
+import com.nguyenmp.cs263_real.dao.UsageDao;
+import com.nguyenmp.cs263_real.model.UsageModel;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.channels.Channels;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * This class converts the "check-in" style data to an interval set.
+ * This hopefully reduces entries for computers that have a lot of
+ * activity and reduce the overall size of the datastore.  We will
+ * prune the datastore to only store 1 day's worth of data.
+ */
+public class DatastoreToBlobstoreConverter extends HttpServlet {
+    private static final String ENCODING = "UTF-8";
+    private static final long TIMEOUT = 11 * 60 * 1000; // 11 minutes in milliseconds
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String computer = req.getParameter("computer");
+
+        // Convert our usages into an interval set
+        UsageModel[] usages = UsageDao.getByComputer(computer);
+        Map<String, LinkedList<Interval>> intervals = convertToIntervals(usages);
+        String json = new Gson().toJson(intervals);
+
+        // Persist that interval set into the blobstore
+        GcsService gcsService = GcsServiceFactory.createGcsService();
+        String objectName = String.format("%s/%s", computer, new SimpleDateFormat().format(new Date()));
+        String bucketName = "mark_nguyen_foo";
+        GcsFilename filename = new GcsFilename(bucketName, objectName);
+        GcsOutputChannel outputChannel = gcsService.createOrReplace(filename, GcsFileOptions.getDefaultInstance());
+        OutputStream outputStream = Channels.newOutputStream(outputChannel);
+        byte[] data = json.getBytes(ENCODING);
+
+        outputStream.write(data);
+        outputStream.close();
+
+        resp.getWriter().println(objectName);
+    }
+
+    private static Map<String, LinkedList<Interval>> convertToIntervals(UsageModel[] usages) {
+
+        HashMap<String, LinkedList<Interval>> users = new HashMap<>();
+        for (UsageModel usage : usages) {
+            LinkedList<Interval> intervals = users.get(usage.username);
+            if (intervals == null) {
+                intervals = new LinkedList<>();
+                users.put(usage.username, intervals);
+            }
+
+            Interval tail = intervals.size() == 0 ? null : intervals.getLast();
+            if (tail == null || tail.end + TIMEOUT < usage.timestamp) {
+                // Either the last interval didn't exist or it timed out.
+                // So now we start a new interval
+                tail = new Interval();
+                tail.start = usage.timestamp;
+                tail.end = usage.timestamp;
+                intervals.add(tail);
+            } else {
+                // Extend the old interval
+                tail.end = usage.timestamp;
+            }
+        }
+
+        return users;
+    }
+
+    public static class Interval {
+        public long start, end;
+    }
+}
---
+++ src/main/java/com/nguyenmp/cs263_real/servlet/PersistTask.java
@@ -0,0 +1,26 @@
+package com.nguyenmp.cs263_real.servlet;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.nguyenmp.cs263_real.dao.UsageDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.google.appengine.api.taskqueue.TaskOptions.Builder.withUrl;
+
+public class PersistTask extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Populate backend queue for each computer
+        String[] computers = UsageDao.getComputers();
+
+        for (String computer : computers) {
+            Queue queue = QueueFactory.getDefaultQueue();
+            queue.add(withUrl("/datastore_to_blobstore").param("computer", computer));
+        }
+    }
+}
